fedbced7dd2cba38ba2678053e49cbca95e3499c
avraham <avrahamleff@gmail.com>
2021-11-02 16:04:47 -0400

    Added stubbed "compareTo", more Javadoc.

--- MergeAnInterval/src/main/java/edu/yu/introtoalgs/MergeAnInterval.java
+++ MergeAnInterval/src/main/java/edu/yu/introtoalgs/MergeAnInterval.java
@@ -2,10 +2,22 @@ package edu.yu.introtoalgs;
 
 import java.util.*;
 
+/** Implements the "Add an Interval To a Set of Intervals" semantics defined in
+ * the requirements document.
+ * 
+ * @author Avraham Leff 
+ */
+
 public class MergeAnInterval {
 
   /** An immutable class, holds a left and right integer-valued pair that
    * defines a closed interval
+   *
+   * IMPORTANT: students may not modify the semantics of the "left", "right"
+   * instance variables, nor may they use any other constructor signature.
+   * Students may (are encouraged to) add any other methods that they choose,
+   * bearing in mind that my tests will ONLY DIRECTLY INVOKE the constructor
+   * and the "merge" method.
    */
   public static class Interval implements Comparable<Interval>{
     public final int left;
@@ -13,28 +25,35 @@ public class MergeAnInterval {
 
     /** Constructor
      * 
-     * @param left the left endpoint of the interval
-     * @param right the right endpoint of the interval
+     * @param left the left endpoint of the interval, may be negative
+     * @param right the right endpoint of the interval, may be negative
+     * @throws IllegalArgumentException if left is >= right
      */
     public Interval(int l, int r) {
       this.left = l;
       this.right = r;
     }
 
-      // fill in the rest yourself!
+    @Override
+    public int	compareTo(Interval o) {
+	// fill me in!
+	return -1;
+    }
+
   } // Interval class
 
   /** Merges the new interval into an existing set of disjoint intervals.
    *
-   * @param intervals the existing set of intervals
+   * @param intervals an set of disjoint intervals (may be empty)
    * @param newInterval the interval to be added
    * @return a new set of disjoint intervals containing the original intervals
    * and the new interval, merging the new interval if necessary into existing
    * interval(s), to preseve the "disjointedness" property.
+   * @throws IllegalArgumentException if either parameter is null
    */
   public static Set<Interval> merge
     (final Set<Interval> intervals, Interval newInterval)
   {
-      // fill in the rest yourself!
+      return null;
   }
 }
