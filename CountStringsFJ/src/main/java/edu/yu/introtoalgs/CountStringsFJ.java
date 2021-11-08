45fdf020e699007eea595257b86eb4036111eed6
avraham <avrahamleff@gmail.com>
2021-11-07 21:20:28 -0500

    Initial commit of CountStringsFJ.

---
+++ CountStringsFJ/src/main/java/edu/yu/introtoalgs/CountStringsFJ.java
@@ -0,0 +1,34 @@
+package edu.yu.introtoalgs;
+
+/** Implements the CountStringsFJ semantics specified in the requirements
+ * document.
+ *
+ * @author Avraham Leff
+ */
+
+public class CountStringsFJ {
+
+  /** Constructor.
+   *
+   * @param arr the array to process, can't be null or empty
+   * @param str the string to match, can't be null, may be empty
+   * @param threshold when the length of arr is less than threshold, processing
+   * must be sequential; otherwise, processing must use a fork/join, recursive
+   * divide-and-conquer strategy.  The parameter must be greater than 0.
+   *
+   * IMPORTANT: Students must use this constructor, they MAY NOT add another
+   * constructor.
+   */
+  public CountStringsFJ(final String[] arr, final String str, final int threshold) {
+      // fill me in!
+  }
+  
+  /** Returns the number of elements in arr that ".equal" the "str" parameter
+   *
+   * @return Using a strategy dictated by the relative values of threshold and
+   * the size of arr, returns the number of times that str appears in arr
+   */
+  public int doIt() {
+      return -1;
+  }
+}
