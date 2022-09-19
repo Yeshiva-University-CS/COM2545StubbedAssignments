7c040f6d0a8791c1026a91b1285c604f2ce8e548
avraham <avrahamleff@gmail.com>
2022-09-19 12:47:30 -0400

    Initial version.

---
+++ WellFormedString/src/main/java/edu/yu/introtoalgs/WellFormedString.java
@@ -0,0 +1,20 @@
+package edu.yu.introtoalgs;
+
+/** Defines the API for the WellFormedString assignment: see the requirements
+ * document for more information.
+ *
+ * Students MAY NOT change the constructor signature.  My test code will only
+ * invoke the API defined below.
+ *
+ * @author Avraham Leff
+ */
+public class WellFormedString {
+  public WellFormedString() {
+    // fill in to taste
+  }
+
+  public boolean isIt(final String s) {
+      // supply a better implementation!
+      return false;
+  }  
+}
