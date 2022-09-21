d9ef687a29ffd911ba0aa75adad8839468bfdc82
avraham <avrahamleff@gmail.com>
2022-09-21 08:49:43 -0400

    Javadoc

--- WellFormedString/src/main/java/edu/yu/introtoalgs/WellFormedString.java
+++ WellFormedString/src/main/java/edu/yu/introtoalgs/WellFormedString.java
@@ -9,12 +9,21 @@ package edu.yu.introtoalgs;
  * @author Avraham Leff
  */
 public class WellFormedString {
-  public WellFormedString() {
-    // fill in to taste
-  }
+    public WellFormedString() {
+	// fill in to taste
+    }
 
-  public boolean isIt(final String s) {
-      // supply a better implementation!
-      return false;
-  }  
+    /** Returns true iff s is a well-formed string by the rules discussed in the
+     * requirements doc, false otherwise.
+     *
+     * @param s the String to be evaluated for well-formedness
+     * @return true iff s is well-formed, false otherwise.
+     *
+     * Note: to avoid religious wars, I commit to not test with empty or null
+     * parameters.
+     */
+    public boolean isIt(final String s) {
+	// supply a better implementation!
+	return false;
+    }  
 }
